8f517232799b35083eeb8b6c93d977df3f574ec9
Mukesh Sinha <mykusdev@gmail.com>
2024-10-29 21:55:03 +0530

    added login validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import axios from 'axios';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 interface user{
   id: string;
@@ -13,13 +17,24 @@ interface user{
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   // username: string = '';
   // password: string = '';
+
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    const localkey = localStorage.getItem('localkey');
+    if(localkey !== null){
+      this.router.navigate(['/dashboard']);
+    }
+  }
+
+
   userdata: user = {
     id: '',
     username: '',
@@ -28,26 +43,61 @@ export class LoginComponent {
     address: '',
     email: ''
   };
+  loginError: string = '';
 
-  onInputChange(event: Event, eventName:string){
-    if(eventName === 'username'){
-      this.userdata.username = (<HTMLInputElement>event.target).value;
+  
+
+  // onInputChange(event: Event, eventName:string){
+  //   if(eventName === 'username'){
+  //     this.userdata.username = (<HTMLInputElement>event.target).value;
+  //   }
+  //   else if(eventName === 'password'){
+  //     this.userdata.password = (<HTMLInputElement>event.target).value;
+  //   }
+  // }
+
+
+
+  onLogin(){
+  
+    if(this.userdata.username === ''){
+      this.loginError = 'Please enter a valid username';
+      setTimeout(() => {
+        this.loginError = '';
+      }, 4000);
     }
-    else if(eventName === 'password'){
-      this.userdata.password = (<HTMLInputElement>event.target).value;
+    else if(this.userdata.password === ''){
+      this.loginError = 'Please enter a valid password';
+      setTimeout(() => {
+        this.loginError = '';
+      }, 4000);
     }
-  }
-
-
-  async onLogin(){
+    else{
+      this.onApiCall();
+    }
     console.log(this.userdata);
+
+    
+    
+  }
+  async onApiCall(){
+    
     const path = `http://localhost:4500/userData?username=${this.userdata.username}`;
     try{
       const response = await axios.get(path);
     console.log(response.data);
+    if(this.userdata.password === response.data[0].password){
+      localStorage.setItem('localkey', response.data[0].username);
+      this.router.navigate(['/dashboard']);
+    }else{
+      this.loginError = 'Wrong Password';
+      setTimeout(() => {
+        this.loginError = '';
+      }, 3000);
+    }
+    
     }catch(err){
       console.log(err);
     }
-    
   }
 }
